dec9a66ccb116f1907c4babef986e87e9c593498
marwaMRadwan <marwaradwan872@hotmail.com>
2022-05-09 10:37:23 +0200

    s2

--- session2/js/tasks.js
+++ session2/js/tasks.js
@@ -1,16 +1,32 @@
 const addForm = document.querySelector("#addForm")
-const allTasks = []
-addForm.addEventListener("submit", function(e){
-    e.preventDefault()
-    task = {
-        title : this.elements.title.value,
-        content : this.elements.content.value,
-        dueDate : this.elements.dueDate.value,
-        status:false,
-        id: Date.now()
+const readFromStorage = () => {
+    let data
+    try{
+        data = JSON.parse(localStorage.getItem("tasks")) || []
+        if(!Array.isArray(data)) throw new Error("no data")
     }
-    allTasks.push(task)
-    localStorage.setItem("tasks", JSON.stringify(allTasks))
-    this.reset()
-    window.location.href="index.html"
-})
+    catch(e){
+        data = []
+    }
+    return data
+}
+const writeToStorage = (data) => localStorage.setItem("tasks", JSON.stringify(data))
+if(addForm){
+    addForm.addEventListener("submit", function(e){
+        e.preventDefault()
+        task = {
+            title : this.elements.title.value,
+            content : this.elements.content.value,
+            dueDate : this.elements.dueDate.value,
+            status:false,
+            id: Date.now()
+        }
+        const allTasks = readFromStorage()
+        allTasks.push(task)
+        writeToStorage(allTasks)
+        this.reset()
+        window.location.href="index.html"
+    })
+    
+}
+
