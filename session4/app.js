63aed5ee75338a742325469701a73271b948188c
marwaMRadwan <marwaradwan872@hotmail.com>
2022-05-12 11:26:14 +0200

    s4

---
+++ session4/app.js
@@ -0,0 +1,33 @@
+// console.log("hello")
+//modules => file created , package built-in , npm
+// const mod = require('./myMod')
+// mod()
+// mod.t()
+// mod.t2()
+
+
+// const fs = require('fs')
+// fs.mkdir("a", ()=>{console.log("done")})
+// fs.writeFileSync("a/data.txt", "hello from node")
+//npm init 
+// const validator = require('validator');
+
+// validator.isEmail('foo@bar.com'); //=> true
+// const validator = require("validator")
+// const chalk = require("chalk")
+// // console.log(chalk.red("hello"))
+// const phoneNum = "01934567896"
+// isPhone = validator.isMobilePhone(phoneNum, ['ar-EG'])
+// isPhone? 
+//     console.log(chalk.green("Valid Number")): 
+//     console.log(chalk.red("Invalid Number"))
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(process.argv)
+let x = process.argv[2]
+x%2==0 ? console.log('even'): console.log("odd");;
+
+//node app 5 / 3
+
