d5e215d971b9015d2b0094a0b82b53ca1aa8859e
marwaMRadwan <marwaradwan872@hotmail.com>
2022-05-12 13:52:59 +0200

    s4

--- session4/yargsApp/controller/users.js
+++ session4/yargsApp/controller/users.js
@@ -1,1 +1,14 @@
-module.exports = {}
+const addUser = (userData) => {
+
+}
+const allUsers = () => {
+
+}
+const singleUser = (userId) => {
+
+}
+const editUser = (userId, newData)=>{
+
+}
+const delUser = (userId)=>{}
+module.exports = {addUser, editUser, allUsers, singleUser, delUser}
--- session4/yargsApp/index.js
+++ session4/yargsApp/index.js
@@ -1,3 +1,63 @@
 //npm init --y
 // npm i yargs validator chalk@4.0.0
 const users = require('./controller/users')
+const yargs = require("yargs")
+
+yargs.command({
+    command:"add",
+    describe:"add new user data",
+    builder:{
+        id:{},
+        name:{},
+        email:{},
+        status:{},
+        age:{},
+        createdAt:{}
+    },
+    handler: function(argv){
+        console.log("add new user");
+    }
+})
+yargs.command({
+    command:"showAll",
+    describe:"show all users data",
+    handler: function(){
+        console.log("show all users");
+    }
+})
+yargs.command({
+    command:"showUser",
+    describe:"show single user data",
+    builder:{
+        id:{}
+    },
+    handler: function(argv){
+        console.log("show single user");
+    }
+})
+yargs.command({
+    command:"delUser",
+    describe:"delete single user data",
+    builder:{
+        id:{}
+    },
+    handler: function(argv){
+        console.log("delete single user");
+    }
+})
+yargs.command({
+    command:"editUser",
+    describe:"show single user data",
+    builder:{
+        userId:{},
+        id:{},
+        name:{},
+        email:{},
+        status:{},
+        age:{}
+    },
+    handler: function(argv){
+        console.log("edit single user");
+    }
+})
+yargs.argv
